e0122c99966c4aa44d6d2014e534b289aee25fb0
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-10-02 15:37:26 +0200

    add the find user by id

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const mongoose = require('mongoose');
-app.use(express.urlencoded({extended: true}))
-const Mydata = require("./models/mySchema"); // this will call ta Schema 
-const { console } = require('inspector');
-
+const express = require("express");
+const app = express();
+const port = 8080;
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mySchema"); // this will call ta Schema
+const { console } = require("inspector");
 
 app.use(express.json());
 
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Matti" });
+});
 
-app.get("/api/home", (req, res)=>{
-  res.json({message:"Matti"})
-})
-
-mongoose.connect(`mongodb+srv://matti:12345@node.9ms98.mongodb.net/alldata?retryWrites=true&w=majority&appName=node`)
-.then(() =>{
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+mongoose
+  .connect(
+    `mongodb+srv://matti:12345@node.9ms98.mongodb.net/alldata?retryWrites=true&w=majority&appName=node`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
   })
-})
-.catch((err)=> {console.log(err)})
+  .catch((err) => {
+    console.log(err);
+  });
 
+// post a user
+app.post("/api/user", async (req, res) => {
+  const mydata = new Mydata();
 
-app.post("/api/user", async(req, res)=>{
-  const mydata = new Mydata()
+  const name = req.body.username;
+  mydata.username = name;
+  await mydata.save();
+  res.json(mydata);
+  console.log(req.body);
+});
+// find all users
+app.get("/api/user", async (req, res) => {
+  const usernames = await Mydata.find();
+  console.log(usernames);
+  res.json(usernames);
+});
 
-  const name = req.body.username
-  mydata.username = name
-  await mydata.save()
-  res.json(mydata)
-  console.log(req.body)
-})
-
-app.get("/api/user", async(req, res)=>{
- const usernames = await Mydata.find()
- console.log(usernames)
- res.json(usernames)
-})
+// find user by id 
+app.get("/api/user/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const username = await Mydata.findById(id);
+    res.status(200).json(username);
+    return;
+  } catch (err) {
+    console.log("error reading userId", id);
+    res.status(500).json({ message: "Server error" });
+  }
+  
+});
